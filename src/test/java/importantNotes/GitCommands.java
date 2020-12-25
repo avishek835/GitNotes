a5d3b9f14c2ed057da3ff4ee6b84c2f3ac75cb70
Avishek <achakraborty83@gmail.com>
2020-12-25 21:47:03 +0530

    Uploading Git Commands

---
+++ src/test/java/importantNotes/GitCommands.java
@@ -0,0 +1,35 @@
+package importantNotes;
+
+public class GitCommands {
+	
+	/*
+	 * C:\Users\Abc>git config --global user.name "Avishek"
+C:\Users\Abc>git config --global user.email "achakraborty83@gmail.com"
+C:\Users\Abc>cd GitCucumberBasics
+C:\Users\Abc\GitCucumberBasics>git init
+C:\Users\Abc\GitCucumberBasics>git add *
+C:\Users\Abc\GitCucumberBasics>git status
+C:\Users\Abc\GitCucumberBasics>git commit -m "first commit"
+C:\Users\Abc\GitCucumberBasics>git remote add origin https://github.com/avishek835/CucumberBasics.git
+C:\Users\Abc\GitCucumberBasics>git push origin master
+C:\Users\Abc\GitCucumberBasics>cd..
+C:\Users\Abc>git clone https://github.com/avishek835/CucumberBasics.git
+C:\Users\Abc>cd C:\Users\Abc\CucumberBasicsY
+C:\Users\Abc\CucumberBasicsY>git add *
+C:\Users\Abc\CucumberBasicsY>git status
+C:\Users\Abc\CucumberBasicsY>git commit -m "this is commit from CucumberBasicsY"
+C:\Users\Abc\CucumberBasicsY>git push origin master
+   C:\Users\Abc\CucumberBasicsY>cd..
+C:\Users\Abc>cd C:\Users\Abc\GitCucumberBasicsX
+C:\Users\Abc\GitCucumberBasicsX>git pull
+C:\Users\Abc\GitCucumberBasicsX>git pull origin master
+From https://github.com/avishek835/CucumberBasics
+C:\Users\Abc\GitCucumberBasicsX>cd E:\
+C:\Users\Abc\GitCucumberBasicsX>cd..
+C:\Users\Abc>cd..
+C:\Users>
+
+	 * 
+	 * */
+
+}
