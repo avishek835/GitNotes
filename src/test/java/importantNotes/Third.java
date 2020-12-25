ed66d53fdd6bcb385ca948932c49351816ca5e13
Avishek <achakraborty83@gmail.com>
2020-12-25 21:45:58 +0530

    uploading Third.java

---
+++ src/test/java/importantNotes/Third.java
@@ -0,0 +1,15 @@
+/*API acts as an interphase/communication protocol between frontend and backend includes Database.
+ * It acts as middle tier solution.
+ * 
+ * Get:- To retrieve any requests.
+ * Post:- To create something on server. eg: html forms
+ * Put:- To update something on server. eg: html forms
+ * Delete: to delete something.
+ * We follow CRUD
+ * 
+ * 
+ * 
+ * 
+ * 
+ * */
+
